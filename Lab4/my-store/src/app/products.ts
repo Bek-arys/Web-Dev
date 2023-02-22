ab0a085df3cbbdc213969930e57f4ff08c1be7de
Bekarys <bekarysnurgassym@gmail.com>
2023-02-22 21:09:46 +0600

    lab4

---
+++ Lab4/my-store/src/app/products.ts
@@ -0,0 +1,42 @@
+export const products = [
+        {
+            id: 1,
+            name: 'Xiaomi Redmi Note 10 Pro',
+            price: 128350,
+            description: 'A large phone with one of the best screens',
+            url: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000#!/item',
+            image: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h59/hcf/64487157956638/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg"
+        },
+        {
+          id: 2,
+          name: 'Apple iPhone 13',
+          price: 372380,
+          description: 'A great phone with one of the best cameras',
+          url: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000#!/item',
+          image: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h01/h10/46392662491166/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg"
+        },
+        {
+          id: 3,
+          name: 'Apple iPhone 14 Pro Max',
+          price: 691680,
+          description: "TOP 1 great phone with one of the best cameras",
+          url: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+          image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h61/62948780736542/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg'
+        },
+        {
+          id: 4,
+          name: 'Xiaomi Redmi 10C',
+          price: 128350,
+          description: 'цвет: серый, объем встроенной памяти: 128 ГБ',
+          url: 'https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-seryi-104417231/?c=750000000#!/item',
+          image: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hea/h96/49939925139486/xiaomi-redmi-10c-4-gb-128-gb-seryj-104417231-1.jpg"
+        },
+        {
+          id: 5,
+          name: 'Phone Standard',
+          price: 299,
+          description: '',
+          url: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000#!/item',
+          image: "https://resources.cdn-kaspi.kz/shop/medias/sys_mas…dmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg"
+        }
+      ];
---
+++ Lab4/my-store/src/app/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  
+  products = [...products];
+
+  share(name: string, url: string | undefined) {
+    window.open(`https://t.me/share/url?url=${url}&text=${name}`)
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
